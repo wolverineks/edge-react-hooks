7c1b731362d98debc9cc1ca4fa488302dde20215
Kevin Sullivan <wolverine.ks@gmail.com>
2018-12-13 16:35:48 -0800

    Add useLocalUsers
    Hack:
    edge-core-js uses an outdated version of flow
    This ignores all of edge-core-js in place of a
    minimal set of temp flow types
    Remove the temp types and use official flow
    types included in edge-core-js once they
    upgrade to ^0.89.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,10 @@ module.exports = {
   extends: [
     'standard',
     'plugin:flowtype/recommended',
-    'plugin:react/recommended',
-    'plugin:react-native/all'
+    'plugin:react/recommended'
   ],
   parser: 'babel-eslint',
-  plugins: ['flowtype', 'standard', 'react', 'react-native', 'react-hooks'],
+  plugins: ['flowtype', 'standard', 'react', 'react-hooks'],
   rules: {
     'flowtype/require-valid-file-annotation': [2, 'always'],
     'flowtype/semi': [2, 'never'],
@@ -25,8 +24,6 @@ module.exports = {
     'no-throw-literal': 'error',
     'no-var': 'error',
     'prefer-const': 'error',
-    'react-native/no-color-literals': 0,
-    'react-native/no-inline-styles': 0,
     'react-hooks/rules-of-hooks': 'error'
   }
 }
