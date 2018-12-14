037042de995e6d1190c627ca86ed692ccde5497f
Kevin Sullivan <wolverine.ks@gmail.com>
2018-12-13 20:23:05 -0800

    Fix exports

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // @flow
 
+import { useActiveWalletIds } from './lib/useActiveWalletIds.js'
+import { useArchivedWalletIds } from './lib/useArchivedWalletIds.js'
+import { useDeletedWalletIds } from './lib/useDeletedWalletIds.js'
 import { useLocalUsers } from './lib/useLocalUsers.js'
+import { useOtpKey } from './lib/useOtpKey.js'
+import { useOtpResetDate } from './lib/useOtpResetDate.js'
 
-export { useLocalUsers }
+export {
+  useLocalUsers,
+  useActiveWalletIds,
+  useArchivedWalletIds,
+  useDeletedWalletIds,
+  useOtpKey,
+  useOtpResetDate
+}
