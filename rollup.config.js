778b56942acd25014857a5ceeeeb444d8f057493
Kevin Sullivan <wolverine.ks@gmail.com>
2018-12-16 14:57:59 -0800

    Enable flow types

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
 import rollupPluginFlow from "rollup-plugin-flow";
 
-export default {
-  input: "src/index.js",
-  output: {
-    file: "dist/index.js",
-    format: "cjs"
-  },
-  plugins: [rollupPluginFlow()]
-};
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs"
+      }
+    ],
+    plugins: [rollupPluginFlow()]
+  }
+];
