0c9d5af9a0711dd2b02c618d2f7bd3bead191cb1
Kevin Sullivan <wolverine.ks@gmail.com>
2018-12-17 17:11:50 -0800

    Fix emports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 // @flow
 
-import { useActiveWalletIds } from './useActiveWalletIds.js'
-import { useArchivedWalletIds } from './useArchivedWalletIds.js'
-import { useDeletedWalletIds } from './useDeletedWalletIds.js'
-import { useLocalUsers } from './useLocalUsers.js'
-import { useOtpKey } from './useOtpKey.js'
-import { useOtpResetDate } from './useOtpResetDate.js'
+import { useActiveWalletIds } from "./useActiveWalletIds.js";
+import { useArchivedWalletIds } from "./useArchivedWalletIds.js";
+import { useDeletedWalletIds } from "./useDeletedWalletIds.js";
+import { useLocalUsers } from "./useLocalUsers.js";
+import { useOtpKey } from "./useOtpKey.js";
+import { useOtpResetDate } from "./useOtpResetDate.js";
+import { useDisklet } from "./useDisklet.js";
 
-export { useActiveWalletIds, useArchivedWalletIds, useDeletedWalletIds, useLocalUsers, useOtpKey, useOtpResetDate }
+export {
+  useActiveWalletIds,
+  useArchivedWalletIds,
+  useDeletedWalletIds,
+  useLocalUsers,
+  useOtpKey,
+  useOtpResetDate,
+  useDisklet
+};
